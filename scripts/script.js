c5516155302a1bc56dc6b9189750ee21f5bddd4a
IronMiron <leon4631991@yandex.ru>
2021-01-01 20:48:14 +0300

    dev/thirdTry

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 let popup = document.querySelector('.popup');
 let editBtn = document.querySelector('.button_type_edit');
 let closeBtn = document.querySelector('.button_type_close');
-let saveBtn = document.querySelector('.button_type_save');
 let profileName = document.querySelector('.profile__name');
 let profileOccupation = document.querySelector('.profile__occupation');
-let popupName = document.querySelector('.popup__name');
-let popupOccupation = document.querySelector('.popup__occupation');
+let popupName = document.querySelector('.popup__input_val_name');
+let popupOccupation = document.querySelector('.popup__input_val_occupation');
 let popupEdit = document.querySelector('.popup__container');
 
 function showEditPopup() {
@@ -18,11 +17,13 @@ function closeEditPopup() {
     popup.classList.remove('popup_opened');
 }
 
-editBtn.addEventListener('click', showEditPopup);
-closeBtn.addEventListener('click', closeEditPopup);
-popupEdit.addEventListener('submit', function saveAndClose(evt) {
+function saveAndClose(evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileOccupation.textContent = popupOccupation.value;
     closeEditPopup();
-});
+}
+
+editBtn.addEventListener('click', showEditPopup);
+closeBtn.addEventListener('click', closeEditPopup);
+popupEdit.addEventListener('submit', saveAndClose);
