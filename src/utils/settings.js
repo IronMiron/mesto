fc10f61d7d23d5fefcd27d1af0bca5439bd03c12
IronMiron <leon4631991@yandex.ru>
2021-04-30 07:53:17 +0300

    Updates

--- src/utils/settings.js
+++
@@ -1,8 +0,0 @@
-export const settings = {
-    formSelector: '.popup__container',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.button_type_save',
-    inactiveButtonClass: '.button_type_disabled',
-    inputErrorClass: '.popup__input_type_error',
-    errorClass: ''
-  }
